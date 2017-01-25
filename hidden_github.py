001cc78a9ded591e3ad9864f49124535c9326224
manpreetsjsu <manpreetsingh_er@yahoo.com>
2017-01-25 03:14:30 -0800

    Add files via upload

---
+++ hidden_github.py
@@ -0,0 +1,8 @@
+# Please Edit this file..Use your own Twitter consumer_key, consumer_secret, token_key, token_secret
+# the given consumer key, token key are false and have been changed for the security purpose and the privacy of the user
+
+def oauth():
+    return {"consumer_key" : "ksdsdkjsdskjdnkjdn6",
+            "consumer_secret" : "ndskjsddsdkjdndskldsnfdlkfnsdlfkdnsklnlY",
+            "token_key" : "2scslcknsdlckdsnc494902hdwfkdfnwklfnklfc",
+            "token_secret" : "dndskldnsdklsdnklnskldnsddklnsfkldsnwskldnkldn"}
